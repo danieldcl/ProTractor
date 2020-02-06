ea965f102bcf4b3d3f8286e0091ead2e378e4e1f
Dingchao Liao <dcliao92@gmail.com>
2020-02-06 15:39:52 -0800

    update header name

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-        header
+        ProTractor
         </header>
         <main>
           {tasks}
